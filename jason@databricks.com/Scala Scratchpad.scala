68f84dea501c0c82f165eabcf85a9c72395f5ca1
pohlposition <pohlposition@users.noreply.github.com>
2015-11-16 13:40:17 -0800

    Remove Databricks Notebook Comments

--- jason@databricks.com/Scala Scratchpad.scala
+++ jason@databricks.com/Scala Scratchpad.scala
@@ -1,25 +1,5 @@
-// Databricks notebook source exported at Mon, 16 Nov 2015 21:34:55 UTC
 val simpleData = Seq(("Group A","Section 1",50),("Group B","Section 2", 75), ("Group A", "Section 1", 25))
-
-// COMMAND ----------
-
 val simpleRdd = sc.parallelize(simpleData)
-
-// COMMAND ----------
-
 val simpleDF = sqlContext.createDataFrame(simpleRdd)
-
-// COMMAND ----------
-
 println("Hello Elizabeth")
-
-// COMMAND ----------
-
 simpleDF.registerTempTable("SimpleTempTable")
-
-// COMMAND ----------
-
-// MAGIC %sql Select * from SimpleTempTable
-
-// COMMAND ----------
-
