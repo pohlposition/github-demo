a111a675ed20087480187aa48bdaf152c7f811c6
pohlposition <pohlposition@users.noreply.github.com>
2015-11-16 13:34:55 -0800

    Added some SQL

--- jason@databricks.com/Scala Scratchpad.scala
+++ jason@databricks.com/Scala Scratchpad.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Mon, 16 Nov 2015 21:15:46 UTC
+// Databricks notebook source exported at Mon, 16 Nov 2015 21:34:55 UTC
 val simpleData = Seq(("Group A","Section 1",50),("Group B","Section 2", 75), ("Group A", "Section 1", 25))
 
 // COMMAND ----------
@@ -12,6 +12,14 @@ val simpleDF = sqlContext.createDataFrame(simpleRdd)
 // COMMAND ----------
 
 println("Hello Elizabeth")
+
+// COMMAND ----------
+
+simpleDF.registerTempTable("SimpleTempTable")
+
+// COMMAND ----------
+
+// MAGIC %sql Select * from SimpleTempTable
 
 // COMMAND ----------
 
