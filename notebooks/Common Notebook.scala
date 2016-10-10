d21113a3c98166b448039565b43dd7eb345c8cab
pohlposition <pohlposition@users.noreply.github.com>
2016-10-10 14:01:11 -0700

    Update Common Notebook.scala

--- notebooks/Common Notebook.scala
+++ notebooks/Common Notebook.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Mon, 10 Oct 2016 21:00:45 UTC
+// Databricks notebook source exported at Mon, 10 Oct 2016 21:01:11 UTC
 // MAGIC %md #dev2 Strikes again!
 
 // COMMAND ----------
