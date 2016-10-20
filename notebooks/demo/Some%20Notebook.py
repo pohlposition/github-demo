3f1f1bdb6e6ab46c10d7c2cf42d357fc4931e5f8
pohlposition <pohlposition@users.noreply.github.com>
2016-10-20 16:58:41 -0700

    updated my private branch

--- notebooks/demo/Some%20Notebook.py
+++ notebooks/demo/Some%20Notebook.py
@@ -1,5 +1,9 @@
-# Databricks notebook source exported at Thu, 20 Oct 2016 23:58:04 UTC
+# Databricks notebook source exported at Thu, 20 Oct 2016 23:58:40 UTC
 1 + 1
+
+# COMMAND ----------
+
+# MAGIC %md Some other stuff
 
 # COMMAND ----------
 
